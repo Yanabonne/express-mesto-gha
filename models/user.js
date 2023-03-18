f288a7b3ad0686a4fff613808cb04851bc861cc3
Yana Morozova <yanabonne@yandex.ru>
2023-03-18 21:49:16 +0300

    feat: configure sending info without __v attribute

--- models/user.js
+++ models/user.js
@@ -1,22 +1,34 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+function omitV(doc, obj) {
+  delete obj.__v;
+  return obj;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    about: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
   },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform: omitV,
+    },
+  }
+);
 
 module.exports = mongoose.model("user", userSchema);
